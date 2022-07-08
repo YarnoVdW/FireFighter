77f6230b70e498c57ee8f204b5232577526db07d
yarno <yarnovandeweyer2@hotmail.com>
2022-07-08 15:47:14 +0200

    first implementations

--- app/src/main/java/com/icapps/yarno/firefighter/FireFighterViewModel.kt
+++ app/src/main/java/com/icapps/yarno/firefighter/FireFighterViewModel.kt
@@ -2,23 +2,34 @@ package com.icapps.yarno.firefighter
 
 import android.app.Application
 import android.os.CountDownTimer
-import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class FireFighterViewModel(application: Application) : AndroidViewModel(application) {
 
-    var gameStarted = false
+
+    private var _gameStarted = MutableLiveData<Boolean>(true)
+    val gameStarted: LiveData<Boolean>
+        get() = _gameStarted
 
 
     private lateinit var countDownTimer: CountDownTimer
-    private val initialCountDownTimerInMillis = 10000L
+    private val initialCountDownTimerInMillis = 5000L
     private val countDownIntervalInMillis = 1000L
 
     private var _timeLeftOnTimer = MutableLiveData<Int>()
     val timeLeftOnTimer: LiveData<Int>
         get() = _timeLeftOnTimer
+
+    private var _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+    private var scoreIncrementer = 0
+    private val context = getApplication<Application>().applicationContext
+
+
 
 
     private var timeLeft = initialCountDownTimerInMillis
@@ -29,7 +40,10 @@ class FireFighterViewModel(application: Application) : AndroidViewModel(applicat
 
     fun resetGame() {
         initCountDownTimer(initialCountDownTimerInMillis)
-        gameStarted = false
+        _gameStarted.postValue(true)
+        _score.value = 0
+
+
     }
 
     private fun initCountDownTimer(timerLeft: Long = initialCountDownTimerInMillis) {
@@ -42,18 +56,29 @@ class FireFighterViewModel(application: Application) : AndroidViewModel(applicat
 
             override fun onFinish() {
                 endGame()
-                gameStarted = false
+                _gameStarted.postValue(true)
             }
         }
     }
 
     fun startGame() {
-        gameStarted = true
+        _gameStarted.postValue(false)
         countDownTimer.start()
-
     }
 
     private fun endGame() {
-
+        Toast.makeText(
+            context,
+            "Times up! You extinguished ${_score.value} flames!",
+            Toast.LENGTH_LONG
+        ).show()
+        resetGame()
     }
+
+    fun flameIsClicked() {
+        scoreIncrementer++
+        _score.postValue(scoreIncrementer)
+    }
+
+
 }
--- app/src/main/java/com/icapps/yarno/firefighter/MainFireFightActivity.kt
+++ app/src/main/java/com/icapps/yarno/firefighter/MainFireFightActivity.kt
@@ -2,15 +2,23 @@ package com.icapps.yarno.firefighter
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.icapps.yarno.firefighter.databinding.ActivityMainFireFightBinding
 
 class MainFireFightActivity : AppCompatActivity() {
-
+    private lateinit var animation: Animation
+    private lateinit var flameImg: ImageView
+    private lateinit var startButton: Button
     private val fireFighterViewModel by lazy {
-         ViewModelProvider(this)[FireFighterViewModel::class.java]
+        ViewModelProvider(this)[FireFighterViewModel::class.java]
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +27,30 @@ class MainFireFightActivity : AppCompatActivity() {
         val binding: ActivityMainFireFightBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main_fire_fight)
 
-        fireFighterViewModel.timeLeftOnTimer.observe(this) {
-            Log.d("Fire", "timeleft: $it")
-        }
+
 
         binding.lifecycleOwner = this
         binding.fireFighterViewModel = fireFighterViewModel
+        flameImg = findViewById(R.id.fire_id)
+        startButton = findViewById(R.id.btn_fighter)
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.dissapear)
+
+
+        startButton.setOnClickListener {
+            startButton.visibility = View.GONE
+        }
+        flameImg.setOnClickListener {
+            val randomInt = (0..1000).random()
+            flameImg.startAnimation(animation)
+            flameImg.animate().x(randomInt.toFloat()).y(randomInt.toFloat()) // window manager en guidelines!
+            fireFighterViewModel.flameIsClicked()
+
+        }
 
         if (savedInstanceState != null) fireFighterViewModel.restoreGame()
         else fireFighterViewModel.resetGame()
 
     }
+
 }
